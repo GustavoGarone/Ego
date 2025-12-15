dda996deb543d64a7bc65fb9de697903dd226d39
Minze <gustavogarone@protonmail.com>
2025-12-15 19:09:02 -0300

    feat: implements tests for basic functions

---
+++ internal/cpu/cpu_test.go
@@ -0,0 +1,62 @@
+package cpu
+
+import (
+	"testing"
+)
+
+func TestLda(t *testing.T) {
+	var accumulatorParam uint8 = 0xc0 // -128, 11000000
+	program := []uint8{0xa9, accumulatorParam}
+	cpu := NewCpu(program)
+	cpu.lda()
+	var expected uint8 = 0b1000_0000
+	if cpu.Status != expected {
+		t.Errorf("LDA failed to update Status register. Got %b want %b", cpu.Status, expected)
+	}
+	if cpu.Accumulator != accumulatorParam {
+		t.Errorf("LDA failed to load to accumulator. Got %x want %x", cpu.Accumulator, accumulatorParam)
+	}
+}
+
+func TestRun(t *testing.T) {
+	var accumulatorParam uint8 = 0xc0
+	program := []uint8{0xa9, 0xc0, 0xaa, 0x00}
+	cpu := NewCpu(program)
+	cpu.Run()
+	if cpu.Accumulator != accumulatorParam {
+		t.Errorf("LDA failed to load to accumulator. Got %x want %x", cpu.Accumulator, accumulatorParam)
+	}
+	if cpu.Accumulator != cpu.X {
+		t.Errorf("Values between accumulator and X differ. Got Acummulator = %x and X = %x", cpu.Accumulator, cpu.X)
+	}
+}
+
+func TestUpdateZeroFlag(t *testing.T) {
+	program := []uint8{0x00}
+	cpu := NewCpu(program)
+	cpu.updateZeroFlag(0)
+	var expect uint8 = 0b0000_0010
+	if cpu.Status != expect {
+		t.Errorf("Failed to update flag. Got %b want %b", cpu.Status, expect)
+	}
+	cpu.updateZeroFlag(1)
+	expect = 0b000_0000
+	if cpu.Status != expect {
+		t.Errorf("Failed to update flag. Got %b want %b", cpu.Status, expect)
+	}
+}
+
+func TestUpdateNegativeFLag(t *testing.T) {
+	program := []uint8{0x00}
+	cpu := NewCpu(program)
+	cpu.updateNegativeFlag(0b1001_1110)
+	var expect uint8 = 0b1000_0000
+	if cpu.Status != expect {
+		t.Errorf("Failed to update flag. Got %b want %b", cpu.Status, expect)
+	}
+	cpu.updateNegativeFlag(1)
+	expect = 0b000_0000
+	if cpu.Status != expect {
+		t.Errorf("Failed to update flag. Got %b want %b", cpu.Status, expect)
+	}
+}
