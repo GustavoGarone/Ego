5cfa7f7e6d47c08257e9f5a0b0f6cb0d1d5d6998
marciosobel <marciosobel@proton.me>
2026-02-02 01:51:04 -0300

    feat: load cartridge from path

--- cmd/ego.go
+++ cmd/ego.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/GustavoGarone/ego/internal/bus"
+	"github.com/GustavoGarone/ego/internal/cartridge"
+	"github.com/GustavoGarone/ego/internal/cpu"
 )
 
 func main() {
-	fmt.Println("Hello flake")
+	if len(os.Args) < 2 {
+		log.Fatal("usage: ego <path>")
+	}
+
+	path := os.Args[1]
+	log.Printf("Loading cartridge with path %s\n", path)
+
+	cart, err := cartridge.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Failed to read cartridge: %v", err)
+	}
+
+	bus := bus.New(cart)
+	cpu := cpu.New(bus)
+
+	cpu.Run()
 }
